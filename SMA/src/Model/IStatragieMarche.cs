65f70f74337c967c1f4844f9af0dc01035ed3413
ggtk <jdbasic@gmail.com>
2013-02-08 15:54:13 +0100

    onch

---
+++ SMA/src/Model/IStatragieMarche.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMA.src.Model
+{
+    class IStatragieMarche
+    {
+        void marcher();
+    }
+}
